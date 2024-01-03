c6008c5ad7c99afabf70d6ddb702d23d1b093044
monayun <yunred9@gmail.com>
2024-01-03 19:58:38 +0900

    [#1] 환경설정 후 페이지 띄우기
    - vue-springBoot 연동 후 페이지 띄우기
    - mysql 연동하기

---
+++ src/main/java/com/spring/book/book_title/BookTitleApplication.java
@@ -0,0 +1,13 @@
+package com.spring.book.book_title;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTitleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookTitleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/book/book_title/BookTitleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.book.book_title;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTitleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
